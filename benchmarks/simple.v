86645730212d37270e0b87b6b7a88a634260e610
Carlos-Jr <diascarlosjr@gmail.com>
2024-03-23 20:15:00 -0300

    Simple.v corrigido

--- benchmarks/simple.v
+++ benchmarks/simple.v
@@ -1,18 +1,15 @@
 //Circuito de exemplo de Designing Partially Reversible FCN Circuits (Chaves,2019)
 module LogicModule (
-    pi0, pi1, pi2,pi3
+    pi0, pi1
     po0, po1
 );
-    input  pi0, pi1, pi2,pi3;
+    input  pi0, pi1;
     output po0, po1;
-    wire   n1, n2, n3;
+    wire   n1,n2,n3;
 
-    //Gate 1
     assign n1 = pi0 & pi1;
-    //Gate 2
-    assign n2 = pi2 & pi3;
-    //Gate 3
-    assign po0 = ~n1 | n2;
-    //Gate 4
-    assign po1 = ~n1 & n2;
+    assign n2 = pi0 & ~n1;
+    assign n3 = pi1 & ~n1;
+    assign po0 = ~n2 & ~n3;
+    assign po1 = ~n3 & pi1;
 endmodule
