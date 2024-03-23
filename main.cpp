9b14fa41cb83d2ad4b689dabac473a25e6fa90e6
Carlos-Jr <diascarlosjr@gmail.com>
2024-03-23 19:17:03 +0000

    Atualizado o método para abrir o arquivo verilog

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "Circuit.h"
 
-int main() {
+#include "Circuit.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <VerilogFileName>" << std::endl;
+        return 1;
+    }
     Circuit circuit;
-    circuit.loadFromVerilog("benchmarks/04-adder.v");
+    circuit.loadFromVerilog(argv[1]);
     circuit.simulate();
     std::cout <<"Entropia:"<<circuit.entropy << std::endl;
     std::cout <<"Tamanho:"<<circuit.gates.size() << std::endl;
