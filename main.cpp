de220d64e691312b6c758bac587b0fc964bc3c72
Carlos-Jr <diascarlosjr@gmail.com>
2024-03-23 22:09:09 -0300

    Alterada Main para receber os argumentos de gerações e parametro a ser otimizado

--- main.cpp
+++ main.cpp
@@ -1,19 +1,47 @@
 #include "Circuit.h"
-
-#include "Circuit.h"
-#include <iostream>
+#include "SimpleCGP.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <VerilogFileName>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <VerilogFileName> [<generations> <parameter>]" << std::endl;
         return 1;
     }
+    
+    if (!std::filesystem::exists(argv[1])) {
+        std::cerr << "File does not exist: " << argv[1] << std::endl;
+        return 1;
+    }
+
     Circuit circuit;
     circuit.loadFromVerilog(argv[1]);
-    circuit.simulate();
-    std::cout <<"Entropia:"<<circuit.entropy << std::endl;
-    std::cout <<"Tamanho:"<<circuit.gates.size() << std::endl;
-    std::cout <<"Depth:"<<circuit.depth << std::endl;
+    circuit.update();
+
+    int runGenerations = 70000;
+    //Argumento numero de gerações
+    if(argc>2)
+        runGenerations = std::stoi(argv[2]);
+    
+    // Argumento de parametro
+    if(argc>3){
+        std::string parameter = argv[3];
+        std::transform(parameter.begin(), parameter.end(), parameter.begin(), ::toupper);
+
+        if(parameter == "ENTROPY"){
+            SimpleCGP ga(runGenerations,circuit,ENTROPY);
+            ga.run();
+        }else if(parameter == "SIZE"){
+            SimpleCGP ga(runGenerations,circuit,SIZE);
+            ga.run();
+        }else if(parameter == "DEPTH"){
+            SimpleCGP ga(runGenerations,circuit,DEPTH);
+            ga.run();
+        }else{
+            std::cerr << "Parameter does not exist\n";
+        }
+    }else{
+        SimpleCGP ga(runGenerations,circuit,ENTROPY);
+        ga.run();
+    }
 
     return 0;
 }
