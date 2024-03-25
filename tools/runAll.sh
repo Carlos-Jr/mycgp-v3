eee5606b636e99d88160e0ea880e5d45049d14d2
Carlos-Jr <diascarlosjr@gmail.com>
2024-03-25 12:35:24 -0300

    Criado script para obter todos os resultados

---
+++ tools/runAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..30}; do
+    for file in ./benchmarks/*; do
+        for parameter in entropy size depth; do
+            bench=$(basename "${file%.*}")
+            echo "./main $file 70000 $parameter >> ./results/$bench-$parameter-$i.csv"
+            ./main $file 70000 $parameter >> "./results/$bench-$parameter-$i.csv"
+        done
+    done
+done
